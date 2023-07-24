8e1cd3a0120399a1f4b8ddea21e70c0f551e7f32
Pablo Brodt <pablobrodt@gmail.com>
2023-07-23 22:35:38 -0300

    feat: add persons getter on project item

--- src/app.ts
+++ src/app.ts
@@ -264,6 +264,17 @@ type ProjectItemParams =
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     private project: Project;
 
+    get persons() {
+        const { people } = this.project
+
+        if (people === 1) {
+            return '1 Pessoa atribuída';
+        }
+
+
+        return `${people} Pessoas atribuídas`;
+    }
+
     constructor({ hostId, project }: ProjectItemParams) {
         super({ hostId, elementId: project.id, templateId: 'single-project' });
 
@@ -275,7 +286,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
     renderContent(): void {
         this.querySelector('h2').textContent = this.project.title;
-        this.querySelector('h3').textContent = this.project.people.toString();
+        this.querySelector('h3').textContent = this.persons;
         this.querySelector('p').textContent = this.project.description;
     }
 }
